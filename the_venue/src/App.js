93d3ed3e8468d46ec46473d5929cc8f006e6a879
Kyle Desktop <kylespresser@gmail.com>
2018-10-10 15:20:41 -0400

    lecture 21

--- the_venue/src/App.js
+++ the_venue/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Element } from 'react-scroll';
+
 import './resources/styles.css';
 
 import Header from './components/header_footer/Header';
@@ -17,11 +19,21 @@ class App extends Component {
         style={{ height: '1500px', background: 'cornflowerblue' }}
       >
         <Header />
-        <Featured />
-        <VenueNfo />
-        <Highlight />
-        <Pricing />
-        <Location />
+        <Element name="featured">
+          <Featured />
+        </Element>
+        <Element name="venuenfo">
+          <VenueNfo />
+        </Element>
+        <Element name="highlights">
+          <Highlight />
+        </Element>
+        <Element name="pricing">
+          <Pricing />
+        </Element>
+        <Element name="location">
+          <Location />
+        </Element>
         <Footer />
       </div>
     );
