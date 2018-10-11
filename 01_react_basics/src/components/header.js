7d17030966aa073b5b8a9c7ca20e172dcd51e123
Kyle Desktop <kylespresser@gmail.com>
2018-10-11 09:45:42 -0400

    lecture 172

--- 01_react_basics/src/components/header.js
+++ 01_react_basics/src/components/header.js
@@ -2,11 +2,15 @@ import React, { Component } from 'react';
 import '../css/styles.css';
 
 class Header extends Component {
+  inputChangeHandler(e) {
+    console.log(e.target.value);
+  }
+
   render() {
     return (
       <header>
         <div className="logo">Logo</div>
-        <input type="text" />
+        <input type="text" onChange={this.inputChangeHandler} />
       </header>
     );
   }
