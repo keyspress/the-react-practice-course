bb41a14c27f75130c4c9faaca0cf419887cf77ff
Kyle Desktop <kylespresser@gmail.com>
2018-10-11 12:10:01 -0400

    lecture 182

--- 01_react_basics/src/components/header.js
+++ 01_react_basics/src/components/header.js
@@ -1,25 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import classes from '../css/styles.css';
 
-class Header extends Component {
-  state = {
-    keywords: ''
-  };
-
-  inputChangeHandler = e => {
-    this.setState({
-      keywords: e.target.value
-    });
-  };
-
-  render() {
-    return (
-      <header className={this.state.active}>
-        <div className={classes.logo}>Logo</div>
-        <input type="text" onChange={this.inputChangeHandler} />
-      </header>
-    );
-  }
-}
+const Header = props => {
+  return (
+    <header>
+      <div className={classes.logo}>Logo</div>
+      <input type="text" onChange={props.keywords} />
+    </header>
+  );
+};
 
 export default Header;
--- 01_react_basics/src/index.js
+++ 01_react_basics/src/index.js
@@ -8,14 +8,28 @@ import NewsList from './components/news_list';
 
 class App extends Component {
   state = {
-    news: JSON
+    news: JSON,
+    filtered: []
+  };
+
+  getKeyword = event => {
+    // console.log(event.target.value);
+    let keyword = event.target.value;
+    let filtered = this.state.news.filter(item => {
+      return item.title.indexOf(keyword) > -1;
+    });
+    this.setState({
+      filtered
+    });
   };
 
   render() {
+    let newsFiltered = this.state.filtered;
+    let newsWhole = this.state.news;
     return (
       <div>
-        <Header />
-        <NewsList news={this.state.news}>
+        <Header keywords={this.getKeyword} />
+        <NewsList news={newsFiltered.length === 0 ? newsWhole : newsFiltered}>
           <h1>The News is:</h1>
         </NewsList>
       </div>
