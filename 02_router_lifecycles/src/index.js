9d58a946623c32b822d97f9aa1796816b4ea63d1
Kyle Desktop <kylespresser@gmail.com>
2018-10-11 13:26:00 -0400

    lecture 184

---
+++ 02_router_lifecycles/src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+import Home from './components/home';
+import Posts from './components/posts';
+import Profile from './components/profiles';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <header>
+          <Link to="/">Home</Link>
+          <Link to="/posts">Posts</Link>
+          <Link
+            to={{
+              pathname: '/profile'
+            }}
+          >
+            Profile
+          </Link>
+        </header>
+        <Route path="/" exact component={Home} />
+        <Route path="/posts" component={Posts} />
+        <Route path="/profile" component={Profile} />
+      </div>
+    </BrowserRouter>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
