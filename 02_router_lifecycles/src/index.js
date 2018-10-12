5d21af084c9ed7b2201ef4b06038cc02e92209ae
Kyle Desktop <kylespresser@gmail.com>
2018-10-12 11:30:02 -0400

    lecture 186

--- 02_router_lifecycles/src/index.js
+++ 02_router_lifecycles/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, NavLink } from 'react-router-dom';
+import { BrowserRouter, Route, NavLink, Switch } from 'react-router-dom';
 
 import Home from './components/home';
 import Posts from './components/posts';
@@ -30,10 +30,12 @@ const App = () => {
             Profile
           </NavLink>
         </header>
-        <Route path="/" exact component={Home} />
-        <Route path="/posts" exact component={Posts} />
-        <Route path="/posts/:id" component={PostItem} />
-        <Route path="/profile" component={Profile} />
+        <Switch>
+          <Route path="/posts/:id" component={PostItem} />
+          <Route path="/posts" component={Posts} />
+          <Route path="/profile" component={Profile} />
+          <Route path="/" component={Home} />
+        </Switch>
       </div>
     </BrowserRouter>
   );
