03d3834bbfd449cc4170125494482895055b27f9
Kyle Desktop <kylespresser@gmail.com>
2018-10-12 12:59:20 -0400

    lecture 192

--- 02_router_lifecycles/src/components/lifecycles.js
+++ 02_router_lifecycles/src/components/lifecycles.js
@@ -1,33 +1,32 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 
-class Life extends Component {
+class Life extends PureComponent {
   // 1 get default props
   // 2 set default state
   state = {
     title: 'Life cycles'
   };
   // 3 before render
-  componentWillMount() {
-    console.log('3 before render: will mount');
-    // document.querySelector('h3').style.color = 'red';
-  }
+  // componentWillMount() {
+  //   console.log('3 before render: will mount');
+  //   // document.querySelector('h3').style.color = 'red';
+  // }
 
-  componentWillUpdate() {
-    console.log('before update');
-  }
-  componentDidUpdate() {
-    console.log('after update');
-  }
-  shouldComponentUpdate(nextProps, nextState) {
-    console.log(nextState.title);
-    if (nextState.title === this.state.title) {
-      return false;
-    }
-    return true;
-  }
-  componentWillReceiveProps(nextProps) {
-    console.log('before recieve props');
-  }
+  // componentWillUpdate() {
+  //   console.log('before update');
+  // }
+  // componentDidUpdate() {
+  //   console.log('after update');
+  // }
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   if (nextState.title === this.state.title) {
+  //     return false;
+  //   }
+  //   return true;
+  // }
+  // componentWillReceiveProps(nextProps) {
+  //   console.log('before recieve props');
+  // }
 
   // 4 render
   render() {
@@ -42,13 +41,13 @@ class Life extends Component {
     );
   }
   // 5 after render
-  componentWillUnmount() {
-    console.log('unmount');
-  }
-  componentDidMount() {
-    console.log('5 after render: did mount');
-    document.querySelector('h3').style.color = 'red';
-  }
+  //   componentWillUnmount() {
+  //     console.log('unmount');
+  //   }
+  //   componentDidMount() {
+  //     console.log('5 after render: did mount');
+  //     document.querySelector('h3').style.color = 'red';
+  //   }
 }
 
 export default Life;
