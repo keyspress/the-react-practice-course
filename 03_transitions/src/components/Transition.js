62409ea77e3e117fddc00d1b3f28b5a86e0edf61
Kyle Desktop <kylespresser@gmail.com>
2018-10-12 15:09:39 -0400

    lecture 196

--- 03_transitions/src/components/Transition.js
+++ 03_transitions/src/components/Transition.js
@@ -1,17 +1,45 @@
 import React, { Component } from 'react';
+import Transition from 'react-transition-group/Transition';
+
 import '../css/App.css';
 
-class TransitionComp extends Component{
+class TransitionComp extends Component {
+  state = {
+    show: true
+  };
 
-    render(){
-        return(
-            <div>
-                Transition
-            </div>
+  showDiv = () => {
+    this.setState({
+      show: !this.state.show
+    });
+  };
 
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Transition
+          in={this.state.show}
+          timeout={2000}
+          mountOnEnter
+          unmountOnExit
+        >
+          {state => (
+            <div
+              style={{
+                background: 'red',
+                height: '100px',
+                transition: 'all 2s ease',
+                opacity: state === 'exited' || state === 'exiting' ? 0 : 1
+              }}
+            />
+          )}
+        </Transition>
+        <div className="showDiv" onClick={this.showDiv}>
+          Show or hide
+        </div>
+      </div>
+    );
+  }
 }
-
 
 export default TransitionComp;
