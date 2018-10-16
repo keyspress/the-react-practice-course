f7a70f677be4bfdfd070eb66689736e13eeb5c77
Kyle Desktop <kylespresser@gmail.com>
2018-10-16 11:46:15 -0400

    lecture 204

--- 05-forms/src/components/user.js
+++ 05-forms/src/components/user.js
@@ -1,18 +1,45 @@
 import React, { Component } from 'react';
 
+import FormFields from '../widgets/Forms/formFields';
+
 class User extends Component {
-
-    state = {
-        
+  state = {
+    formData: {
+      name: {
+        element: 'input',
+        value: '',
+        label: true,
+        labelText: 'Name',
+        config: {
+          name: 'name_input',
+          type: 'text',
+          placeholder: 'Enter your name'
+        }
+      },
+      lastname: {
+        element: 'input',
+        value: '',
+        label: true,
+        labelText: 'Last Name',
+        config: {
+          name: 'lastname_input',
+          type: 'text',
+          placeholder: 'Enter your last name'
+        }
+      }
     }
+  };
 
-    render(){
-        return(
-            <div>
-                User
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="container">
+        <form onSubmit={this.submitForm}>
+          <FormFields formData={this.state.formData} />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default User;
