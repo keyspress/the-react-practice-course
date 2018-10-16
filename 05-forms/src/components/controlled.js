ec0538c5a8aaa9a0c10bae358ad6d13ac42a8124
Kyle Desktop <kylespresser@gmail.com>
2018-10-16 11:26:01 -0400

    lecture 203

--- 05-forms/src/components/controlled.js
+++ 05-forms/src/components/controlled.js
@@ -1,18 +1,53 @@
 import React, { Component } from 'react';
 
 class Controlled extends Component {
+  state = {
+    name: '',
+    lastname: ''
+  };
 
-    state = {
-        
-    }
+  handleNameChange = e => {
+    this.setState({
+      name: e.target.value
+    });
+  };
 
-    render(){
-        return(
-            <div>
-                Controlled
-            </div>
-        )
-    }
+  handleLastNameChange = e => {
+    this.setState({
+      lastname: e.target.value
+    });
+  };
+
+  onshandler = e => {
+    e.preventDefault();
+    console.log(this.state);
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <form onSubmit={this.onshandler}>
+          <div className="form_element">
+            <label>Enter name</label>
+            <input
+              type="text"
+              onChange={this.handleNameChange}
+              value={this.state.name}
+            />
+          </div>
+          <div className="form_element">
+            <label>Enter last name</label>
+            <input
+              type="text"
+              onChange={this.handleLastNameChange}
+              value={this.state.lastname}
+            />
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default Controlled;
--- 05-forms/src/components/uncontrolled.js
+++ 05-forms/src/components/uncontrolled.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
 
 class Uncontrolled extends Component {
+  handleSubmitClick = e => {
+    e.preventDefault();
+    const values = {
+      name: this.name.value,
+      lastname: this.lastname.value
+    };
+    console.log(values);
+  };
 
-    state = {
-
-    }
-
-    render(){
-        return(
-            <div>
-                Uncontrolled
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="container">
+        <form>
+          <div className="form_element">
+            <label>Enter name</label>
+            <input type="text" ref={input => (this.name = input)} />
+          </div>
+          <div className="form_element">
+            <label>Enter last name</label>
+            <input type="text" ref={input => (this.lastname = input)} />
+          </div>
+          <button onClick={this.handleSubmitClick}>Sign in</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default Uncontrolled;
