a664b59c15f1f5709d8c6935c9b695cd336afb44
Kyle Desktop <kylespresser@gmail.com>
2018-10-17 14:22:34 -0400

    lecture 215

--- redux_practice/src/index.js
+++ redux_practice/src/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import Routes from './routes'
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import promiseMiddleware from 'redux-promise';
 
-const App = () => {
-    return(
-        <BrowserRouter>
-            <Routes/>
-        </BrowserRouter>
-    )
-}
+import Routes from './routes';
+import reducers from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware)(
+  createStore
+);
 
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
+);
