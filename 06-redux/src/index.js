e25b97c679fc80e0a324384f7ec4b9769a6a7913
Kyle Desktop <kylespresser@gmail.com>
2018-10-17 12:24:07 -0400

    lecture 211

--- 06-redux/src/index.js
+++ 06-redux/src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
+import reducers from './reducers';
+
 const App = () => <div>Hello</div>;
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware()}>
+  <Provider store={createStoreWithMiddleware(reducers)}>
     <App />
   </Provider>,
   document.getElementById('root')
