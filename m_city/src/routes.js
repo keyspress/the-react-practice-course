5a243ee9f8362203150c01794386ee2a44acea99
Kyle Desktop <kylespresser@gmail.com>
2018-10-18 10:48:22 -0400

    lecture 26

---
+++ m_city/src/routes.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Layout from './Hoc/Layout';
+
+const Routes = props => {
+  return (
+    <Layout>
+      <h1>2nd line</h1>
+    </Layout>
+  );
+};
+
+export default Routes;
