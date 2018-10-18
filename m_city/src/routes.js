0b14a77e0c98f72239e4fb631edb178380dd42ed
Kyle Desktop <kylespresser@gmail.com>
2018-10-18 11:20:41 -0400

    lecture 28

--- m_city/src/routes.js
+++ m_city/src/routes.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 import Layout from './Hoc/Layout';
+import Home from './Components/home';
 
 const Routes = props => {
   return (
     <Layout>
-      <h1>2nd line</h1>
+      <Switch>
+        <Route exact component={Home} path="/" />
+      </Switch>
     </Layout>
   );
 };
