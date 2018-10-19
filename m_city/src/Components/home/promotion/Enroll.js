b2a14769fada681abb77384ec80e2ddcef768310
Kyle Desktop <kylespresser@gmail.com>
2018-10-19 15:30:09 -0400

    lecture 42

--- m_city/src/Components/home/promotion/Enroll.js
+++ m_city/src/Components/home/promotion/Enroll.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Fade from 'react-reveal/Fade';
 
+import { firebasePromotions } from '../../../firebase';
 import { validate } from '../../ui/misc';
 
 import FormField from '../../ui/formFields';
@@ -40,14 +41,70 @@ class Enroll extends Component {
 
     newFormdata[element.id] = newElement;
 
-    console.log(newFormdata);
-
     this.setState({
+      formError: false,
       formdata: newFormdata
     });
   }
 
-  submitForm(event) {}
+  resetFormSuccess(type) {
+    const newFormdata = { ...this.state.formdata };
+
+    for (let key in newFormdata) {
+      newFormdata[key].value = '';
+      newFormdata[key].valid = false;
+      newFormdata[key].validationMessage = '';
+    }
+
+    this.setState({
+      formError: false,
+      formdata: newFormdata,
+      formSuccess: type ? 'Congratulations' : 'Already on the database'
+    });
+
+    this.successMessage();
+  }
+
+  successMessage() {
+    setTimeout(() => {
+      this.setState({
+        formSuccess: ''
+      });
+    }, 2000);
+  }
+
+  submitForm(event) {
+    event.preventDefault();
+
+    let dataToSubmit = {};
+    let formIsValid = true;
+
+    for (let key in this.state.formdata) {
+      dataToSubmit[key] = this.state.formdata[key].value;
+      formIsValid = this.state.formdata[key].valid && formIsValid;
+    }
+
+    if (formIsValid) {
+      firebasePromotions
+        .orderByChild('email')
+        .equalTo(dataToSubmit.email)
+        .once('value')
+        .then(snapshot => {
+          if (snapshot.val() === null) {
+            firebasePromotions.push(dataToSubmit);
+            this.resetFormSuccess(true);
+          } else {
+            this.resetFormSuccess(false);
+          }
+        });
+
+      // this.resetFormSuccess();
+    } else {
+      this.setState({
+        formError: true
+      });
+    }
+  }
 
   render() {
     return (
@@ -61,6 +118,15 @@ class Enroll extends Component {
                 formdata={this.state.formdata.email}
                 change={element => this.updateForm(element)}
               />
+              {this.state.formError ? (
+                <div className="error_label">Something is wrong, try again</div>
+              ) : null}
+              <div className="success_label">{this.state.formSuccess}</div>
+              <button onClick={event => this.submitForm(event)}>Enroll</button>
+              <div className="enroll_disclaimer">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit,
+                quia!
+              </div>
             </div>
           </form>
         </div>
--- m_city/src/firebase.js
+++ m_city/src/firebase.js
@@ -17,5 +17,6 @@ firebase.initializeApp(config);
 
 const firebaseDB = firebase.database();
 const firebaseMatches = firebaseDB.ref('matches');
+const firebasePromotions = firebaseDB.ref('promotions');
 
-export { firebase, firebaseMatches };
+export { firebase, firebaseMatches, firebasePromotions };
