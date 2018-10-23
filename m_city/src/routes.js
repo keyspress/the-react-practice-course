684fd923267dbee19a09840bb365fe870cf0bae6
Kyle Desktop <kylespresser@gmail.com>
2018-10-23 10:39:05 -0400

    lecture 46

--- m_city/src/routes.js
+++ m_city/src/routes.js
@@ -4,6 +4,7 @@ import { Switch, Route } from 'react-router-dom';
 import Layout from './Hoc/Layout';
 import Home from './Components/home';
 import SignIn from './Components/signin';
+import PrivateRoute from './Components/authRoutes/privateRoutes';
 
 import Dashboard from './Components/admin/Dashboard';
 
@@ -11,7 +12,12 @@ const Routes = props => {
   return (
     <Layout>
       <Switch>
-        <Route exact component={Dashboard} path="/dashboard" />
+        <PrivateRoute
+          {...props}
+          path="/dashboard"
+          exact
+          component={Dashboard}
+        />
         <Route exact component={SignIn} path="/sign_in" />
         <Route exact component={Home} path="/" />
       </Switch>
