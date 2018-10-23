cd1de1e4ba8f22387eaa37413ad55d04653ef516
Kyle Desktop <kylespresser@gmail.com>
2018-10-23 11:56:17 -0400

    lecture 49

--- m_city/src/routes.js
+++ m_city/src/routes.js
@@ -9,11 +9,18 @@ import PublicRoute from './Components/authRoutes/publicRoutes';
 
 import Dashboard from './Components/admin/Dashboard';
 import AdminMatches from './Components/admin/matches';
+import AddEditMatch from './Components/admin/matches/addEditMatch';
 
 const Routes = props => {
   return (
     <Layout>
       <Switch>
+        <PrivateRoute
+          {...props}
+          path="/admin_matches/edit_match/:id"
+          exact
+          component={AddEditMatch}
+        />
         <PrivateRoute
           {...props}
           path="/admin_matches"
