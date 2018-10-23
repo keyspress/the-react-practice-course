e049a56136915ac06f9a06e43148fe70612c775f
Kyle Desktop <kylespresser@gmail.com>
2018-10-23 09:35:21 -0400

    lecture 45

---
+++ m_city/src/Components/admin/Dashboard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import AdminLayout from '../../Hoc/AdminLayout';
+
+const Dashboard = () => {
+  return (
+    <AdminLayout>
+      <div className="user_dashboard">
+        <h2>This is your dashboard</h2>
+      </div>
+    </AdminLayout>
+  );
+};
+
+export default Dashboard;
