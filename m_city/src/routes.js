320aebaf3db4ebd385c0a9a3a54ae84f6fc64627
Kyle Desktop <kylespresser@gmail.com>
2018-10-23 10:50:58 -0400

    lecture 47

--- m_city/src/routes.js
+++ m_city/src/routes.js
@@ -5,6 +5,7 @@ import Layout from './Hoc/Layout';
 import Home from './Components/home';
 import SignIn from './Components/signin';
 import PrivateRoute from './Components/authRoutes/privateRoutes';
+import PublicRoute from './Components/authRoutes/publicRoutes';
 
 import Dashboard from './Components/admin/Dashboard';
 
@@ -18,8 +19,20 @@ const Routes = props => {
           exact
           component={Dashboard}
         />
-        <Route exact component={SignIn} path="/sign_in" />
-        <Route exact component={Home} path="/" />
+        <PublicRoute
+          {...props}
+          restricted={true}
+          path="/sign_in"
+          exact
+          component={SignIn}
+        />
+        <PublicRoute
+          {...props}
+          restricted={false}
+          path="/"
+          exact
+          component={Home}
+        />
       </Switch>
     </Layout>
   );
