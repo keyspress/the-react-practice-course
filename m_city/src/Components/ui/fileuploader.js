1ab5d94248eef179a927b1eeedf9e7f0ed6ce6e4
Kyle Desktop <kylespresser@gmail.com>
2018-10-24 09:27:23 -0400

    lecture 56

--- m_city/src/Components/ui/fileuploader.js
+++ m_city/src/Components/ui/fileuploader.js
@@ -11,6 +11,33 @@ class Fileuploader extends Component {
     fileURL: ''
   };
 
+  handleUploadStart = () => {
+    this.setState({
+      isUploading: true
+    });
+  };
+
+  handleUploadError = () => {
+    this.setState({
+      isUploading: false
+    });
+  };
+
+  handleUploadSuccess = filename => {
+    this.setState({
+      name: filename,
+      isUploading: false
+    });
+    firebase
+      .storage()
+      .ref(this.props.dir)
+      .child(filename)
+      .getDownloadURL()
+      .then(url => {
+        this.setState({ fileURL: url });
+      });
+  };
+
   static getDerivedStateFromProps(props, state) {
     if (props.defaultImg) {
       return (state = {
@@ -27,6 +54,35 @@ class Fileuploader extends Component {
         {!this.state.fileURL ? (
           <div>
             <div className="label_inputs">{this.props.tag}</div>
+            <FileUploader
+              accept="image/*"
+              name="image"
+              randomizeFilename
+              storageRef={firebase.storage().ref(this.props.dir)}
+              onUploadStart={this.handleUploadStart}
+              onUploadError={this.handleUploadError}
+              onUploadSuccess={this.handleUploadSuccess}
+            />
+          </div>
+        ) : null}
+        {this.state.isUploading ? (
+          <div
+            className="progress"
+            style={{ textAlign: 'center', margin: '30px 0' }}
+          >
+            <CircularProgress style={{ color: '#98c6e9' }} thickness={7} />
+          </div>
+        ) : null}
+        {this.state.fileURL ? (
+          <div className="image_upload_container">
+            <img
+              style={{ width: '100%' }}
+              src={this.state.fileURL}
+              alt={this.state.name}
+            />
+            <div className="remove" onClick={() => this.uploadAgain()}>
+              Remove
+            </div>
           </div>
         ) : null}
       </div>
