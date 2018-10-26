f6d5c0803739c1a7f6c472feb5d26b85e27d2cde
Kyle Desktop <kylespresser@gmail.com>
2018-10-26 14:55:22 -0400

    lecture 92

--- waves/client/src/components/Register_login/login.js
+++ waves/client/src/components/Register_login/login.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import { update } from '../utils/Form/formActions';
+
 import FormField from '../utils/Form/formfield';
 
 class Login extends Component {
@@ -42,7 +44,13 @@ class Login extends Component {
     }
   };
 
-  updateForm = () => {};
+  updateForm = element => {
+    const newFormdata = update(element, this.state.formdata, 'login');
+    this.setState({
+      formError: false,
+      formdata: newFormdata
+    });
+  };
 
   submitForm = () => {};
 
@@ -53,6 +61,11 @@ class Login extends Component {
           <FormField
             id={'email'}
             formdata={this.state.formdata.email}
+            change={element => this.updateForm(element)}
+          />
+          <FormField
+            id={'password'}
+            formdata={this.state.formdata.password}
             change={element => this.updateForm(element)}
           />
         </form>
--- waves/client/src/components/utils/Form/formfield.js
+++ waves/client/src/components/utils/Form/formfield.js
@@ -1,6 +1,17 @@
 import React from 'react';
 
 export default function FormField({ formdata, change, id }) {
+  const showError = () => {
+    let errorMessage = null;
+    if (formdata.validation && !formdata.valid) {
+      errorMessage = (
+        <div className="error_label">{formdata.validationMessage}</div>
+      );
+    }
+
+    return errorMessage;
+  };
+
   const renderTemplate = () => {
     let formTemplate = null;
 
@@ -14,6 +25,7 @@ export default function FormField({ formdata, change, id }) {
               onBlur={event => change({ event, id, blur: true })}
               onChange={event => change({ event, id })}
             />
+            {showError()}
           </div>
         );
         break;
