149585ce5111ab796a5fc1e703762213afc59a65
Kyle Desktop <kylespresser@gmail.com>
2018-10-30 13:15:21 -0400

    lecture 99

--- waves/client/src/components/Header_footer/Header/index.js
+++ waves/client/src/components/Header_footer/Header/index.js
@@ -1,6 +1,72 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-export default class Header extends Component {
+class Header extends Component {
+  state = {
+    page: [
+      {
+        name: 'Home',
+        linkTo: '/',
+        public: true
+      },
+      {
+        name: 'Guitars',
+        linkTo: '/shop',
+        public: true
+      }
+    ],
+    user: [
+      {
+        name: 'My Cart',
+        linkTo: '/user/cart',
+        public: false
+      },
+      {
+        name: 'My Account',
+        linkTo: '/user/dashboard',
+        public: false
+      },
+      {
+        name: 'Log in',
+        linkTo: '/register_login',
+        public: true
+      },
+      {
+        name: 'Log out',
+        linkTo: '/user/logout',
+        public: false
+      }
+    ]
+  };
+
+  defaultLink = (item, i) => (
+    <Link to={item.linkTo} key={i}>
+      {item.name}
+    </Link>
+  );
+
+  showLinks = type => {
+    let list = [];
+
+    if (this.props.user.userData) {
+      type.forEach(item => {
+        if (!this.props.user.userData.isAuth) {
+          if (item.public === true) {
+            list.push(item);
+          }
+        } else {
+          if (item.name !== 'Log in') {
+            list.push(item);
+          }
+        }
+      });
+    }
+    return list.map((item, i) => {
+      return this.defaultLink(item, i);
+    });
+  };
+
   render() {
     return (
       <header className="bck_b_light">
@@ -9,11 +75,19 @@ export default class Header extends Component {
             <div className="logo">WAVES</div>
           </div>
           <div className="right">
-            <div className="top">LINKS</div>
-            <div className="bottom">LINKS</div>
+            <div className="top">{this.showLinks(this.state.user)}</div>
+            <div className="bottom">{this.showLinks(this.state.page)}</div>
           </div>
         </div>
       </header>
     );
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    user: state.user
+  };
+}
+
+export default connect(mapStateToProps)(Header);
