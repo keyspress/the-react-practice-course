be02ce141a9f7b27a01a0295f5435d8feeec986f
Kyle Desktop <kylespresser@gmail.com>
2018-10-30 21:43:36 -0400

    lecture 100

--- waves/server/server.js
+++ waves/server/server.js
@@ -146,7 +146,7 @@ app.get('/api/users/auth', auth, (req, res) => {
   });
 });
 
-app.get('/api/user/logout', auth, (req, res) => {
+app.get('/api/users/logout', auth, (req, res) => {
   User.findOneAndUpdate({ _id: req.user._id }, { token: '' }, (err, doc) => {
     if (err) return res.json({ success: false, err });
     return res.status(200).send({
