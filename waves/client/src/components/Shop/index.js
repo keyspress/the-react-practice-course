b5318266a844705d8e73c8b3d6fefaa43333e5d5
Kyle Desktop <kylespresser@gmail.com>
2018-10-31 14:18:04 -0400

    lecture 106

--- waves/client/src/components/Shop/index.js
+++ waves/client/src/components/Shop/index.js
@@ -4,12 +4,15 @@ import { connect } from 'react-redux';
 import { getBrands, getWoods } from '../../actions/products_actions';
 
 import PageTop from '../utils/page_top';
+import CollapseCheckbox from '../utils/collapseCheckbox';
 
 class Shop extends Component {
   componentDidMount() {
     this.props.dispatch(getBrands());
     this.props.dispatch(getWoods());
   }
+
+  handleFilters = () => {};
 
   render() {
     const products = this.props.products;
@@ -18,7 +21,14 @@ class Shop extends Component {
         <PageTop title="Browse Products" />
         <div className="container">
           <div className="shop_wrapper">
-            <div className="left">Left</div>
+            <div className="left">
+              <CollapseCheckbox
+                initState={true}
+                title="Brands"
+                list={products.brands}
+                handleFilters={filters => this.handleFilters(filters, 'brand')}
+              />
+            </div>
             <div className="right">Right</div>
           </div>
         </div>
