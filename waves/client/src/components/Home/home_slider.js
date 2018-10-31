0ac3558dd38b3b7b5ad0fbad096424f4765d24b8
Kyle Desktop <kylespresser@gmail.com>
2018-10-31 09:27:52 -0400

    lecture 101

---
+++ waves/client/src/components/Home/home_slider.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import Slider from 'react-slick';
+
+import Mybutton from '../utils/button';
+import MyButton from '../utils/button';
+
+export default function HomeSlider(props) {
+  const slides = [
+    {
+      img: '/images/featured/featured_home.jpg',
+      lineOne: 'Fender',
+      lineTwo: 'Custom shop',
+      linkTitle: 'Shop now',
+      linkTo: '/shop'
+    },
+    {
+      img: '/images/featured/featured_home_2.jpg',
+      lineOne: 'B-Stock',
+      lineTwo: 'Awesome discounts',
+      linkTitle: 'View offers',
+      linkTo: '/shop'
+    }
+  ];
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false
+  };
+
+  const generateSlides = () =>
+    slides
+      ? slides.map((item, i) => (
+          <div key={i}>
+            <div
+              className="featured_image"
+              style={{
+                background: `url(${item.img})`,
+                height: `${window.innerHeight}px`
+              }}
+            >
+              <div className="featured_action">
+                <div className="tag title">{item.lineOne}</div>
+                <div className="tag low_title">{item.lineTwo}</div>
+                <div>
+                  <MyButton
+                    type="default"
+                    title={item.linkTitle}
+                    linkTo={item.linkTo}
+                    addSTyles={{
+                      margin: '10px 0 0 0'
+                    }}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        ))
+      : null;
+
+  return (
+    <div className="featured_container">
+      <Slider {...settings}>{generateSlides()}</Slider>
+    </div>
+  );
+}
--- waves/client/src/components/Home/index.js
+++ waves/client/src/components/Home/index.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 
+import HomeSlider from './home_slider';
+import HomePromotion from './home_promotion';
+
 export default class Home extends Component {
   render() {
     return (
       <div>
-        <h1>Home</h1>
+        <HomeSlider />
+        <HomePromotion />
       </div>
     );
   }
