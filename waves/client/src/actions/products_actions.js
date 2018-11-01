a41b47f5635abddee70802d83cf0692b52863357
Kyle Desktop <kylespresser@gmail.com>
2018-11-01 11:26:23 -0400

    lecture 112

--- waves/client/src/actions/products_actions.js
+++ waves/client/src/actions/products_actions.js
@@ -43,9 +43,10 @@ export function getProductsToShop(
     filters
   };
   const request = axios.post(`${PRODUCT_SERVER}/shop`, data).then(response => {
+    let newState = [...previousState, ...response.data.articles];
     return {
       size: response.data.size,
-      articles: response.data.articles
+      articles: newState
     };
   });
 
--- waves/client/src/components/Shop/loadmoreCards.js
+++ waves/client/src/components/Shop/loadmoreCards.js
@@ -8,6 +8,11 @@ export default function LoadmoreCards(props) {
       <div>
         <CardBlockShop grid={props.grid} list={props.products} />
       </div>
+      {props.size > 0 && props.size >= props.limit ? (
+        <div className="load_more_container">
+          <span onClick={() => props.loadMore()}>Load More</span>
+        </div>
+      ) : null}
     </div>
   );
 }
