e00835acc8b51002a9d69480a8dc936c3c4ab1dd
Kyle Desktop <kylespresser@gmail.com>
2018-11-02 12:32:32 -0400

    lecture 122

--- waves/client/src/components/User/Admin/manage_brands.js
+++ waves/client/src/components/User/Admin/manage_brands.js
@@ -7,7 +7,7 @@ import {
   isFormValid,
   resetFields
 } from '../../utils/Form/formActions';
-import { getBrands } from '../../../actions/products_actions';
+import { getBrands, addBrand } from '../../../actions/products_actions';
 
 import FormField from '../../utils/Form/formfield';
 
@@ -51,14 +51,30 @@ class ManageBrands extends Component {
     });
   };
 
+  resetFieldsHandler = () => {
+    const newFormData = resetFields(this.state.formdata, 'brands');
+    this.setState({
+      formdata: newFormData,
+      formSuccess: true
+    });
+  };
+
   submitForm = event => {
     event.preventDefault();
 
     let dataToSubmit = generateData(this.state.formdata, 'brands');
     let formIsValid = isFormValid(this.state.formdata, 'brands');
-
+    let existingBrands = this.props.products.brands;
     if (formIsValid) {
-      console.log(dataToSubmit);
+      this.props
+        .dispatch(addBrand(dataToSubmit, existingBrands))
+        .then(response => {
+          if (response.payload.success) {
+            this.resetFieldsHandler();
+          } else {
+            this.setState({ formError: true });
+          }
+        });
     } else {
       this.setState({
         formError: true
