5871930c5d0f29ddbfb3a01029f733592cb7fe30
Kyle Desktop <kylespresser@gmail.com>
2018-11-02 13:26:30 -0400

    lecture 124

--- waves/client/src/components/Product/prodNfo.js
+++ waves/client/src/components/Product/prodNfo.js
@@ -7,6 +7,71 @@ import faTimes from '@fortawesome/fontawesome-free-solid/faTimes';
 import MyButton from '../utils/button';
 
 export default function ProdNfo(props) {
+  const showProdTags = detail => (
+    <div className="product_tags">
+      {detail.shipping ? (
+        <div className="tag">
+          <div>
+            <FontAwesomeIcon icon={faTruck} />
+          </div>
+          <div className="tag_text">
+            <div>Free shipping</div>
+            <div>and returns</div>
+          </div>
+        </div>
+      ) : null}
+      {detail.available ? (
+        <div className="tag">
+          <div>
+            <FontAwesomeIcon icon={faCheck} />
+          </div>
+          <div className="tag_text">
+            <div>Available</div>
+            <div>in store</div>
+          </div>
+        </div>
+      ) : (
+        <div className="tag">
+          <div>
+            <FontAwesomeIcon icon={faTimes} />
+          </div>
+          <div className="tag_text">
+            <div>Not Available</div>
+            <div>Preorder only</div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+
+  const showProdActions = detail => (
+    <div className="product_actions">
+      <div className="price">$ {detail.price}</div>
+      <div className="cart">
+        <MyButton
+          type="add_to_cart_link"
+          runAction={() => {
+            console.log('add to cart');
+          }}
+        />
+      </div>
+    </div>
+  );
+
+  const showProdSpecifications = detail => (
+    <div className="product_specifications">
+      <h2>Specs</h2>
+      <div>
+        <div className="item">
+          <strong>Frets:</strong> {detail.frets}
+        </div>
+        <div className="item">
+          <strong>Wood:</strong> {detail.wood.name}
+        </div>
+      </div>
+    </div>
+  );
+
   const detail = props.detail;
   return (
     <div>
@@ -14,6 +79,9 @@ export default function ProdNfo(props) {
         {detail.brand.name} {detail.name}
       </h1>
       <p>{detail.description}</p>
+      {showProdTags(detail)}
+      {showProdActions(detail)}
+      {showProdSpecifications(detail)}
     </div>
   );
 }
