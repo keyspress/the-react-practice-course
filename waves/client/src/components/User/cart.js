513fa9762b9a8425a350957bdfd859bad3572afd
Kyle Desktop <kylespresser@gmail.com>
2018-11-06 12:17:34 -0500

    lecture 131

--- waves/client/src/components/User/cart.js
+++ waves/client/src/components/User/cart.js
@@ -7,6 +7,7 @@ import faSmile from '@fortawesome/fontawesome-free-solid/faSmile';
 import { getCartItems } from '../../actions/user_actions';
 
 import UserLayout from '../../hoc/user';
+import UserProductBlock from '../utils/User/product_block';
 
 class UserCart extends Component {
   state = {
@@ -17,26 +18,35 @@ class UserCart extends Component {
   };
 
   componentDidMount() {
-    let cartItem = [];
+    let cartItems = [];
     let user = this.props.user;
 
     if (user.userData.cart) {
       if (user.userData.cart.length > 0) {
         user.userData.cart.forEach(item => {
-          cartItem.push(item.id);
+          cartItems.push(item.id);
         });
 
-        this.props.dispatch(getCartItems(cartItem, user.userData.cart));
+        this.props.dispatch(getCartItems(cartItems, user.userData.cart));
         // .then(() => {});
       }
     }
   }
 
+  removeFromCart = () => [];
+
   render() {
     return (
       <UserLayout>
         <div>
-          <h1>cart</h1>
+          <h1>My cart</h1>
+          <div className="user_cart">
+            <UserProductBlock
+              products={this.props.user}
+              type="cart"
+              removeItem={id => this.removeFromCart(id)}
+            />
+          </div>
         </div>
       </UserLayout>
     );
