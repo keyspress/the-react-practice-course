9372a9b20f87a8b21aebb3b0e91f64cb896a90f9
Kyle Desktop <kylespresser@gmail.com>
2018-11-07 15:13:09 -0500

    lecture 142

---
+++ waves/client/src/components/User/Admin/manage_site.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import UserLayout from '../../../hoc/user';
+import UpdateSiteNfo from './update_site_nfo';
+
+export default function ManageSite() {
+  return (
+    <UserLayout>
+      <UpdateSiteNfo />
+    </UserLayout>
+  );
+}
--- waves/client/src/routes.js
+++ waves/client/src/routes.js
@@ -14,6 +14,7 @@ import AddProduct from './components/User/Admin/add_product';
 import ManageCategories from './components/User/Admin/manage_categories';
 import UserCart from './components/User/cart';
 import UpdateProfile from './components/User/update_profile';
+import ManageSite from './components/User/Admin/manage_site';
 
 const Routes = () => {
   return (
@@ -39,6 +40,11 @@ const Routes = () => {
           path="/admin/manage_categories"
           exact
           component={Auth(ManageCategories, true)}
+        />
+        <Route
+          path="/admin/site_info"
+          exact
+          component={Auth(ManageSite, true)}
         />
         <Route
           path="/product_detail/:id"
